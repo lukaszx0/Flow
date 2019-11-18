0238d90010c9048cf14d5314775e9e03905db0a9
Lukasz Strzalkowski <lukasz.strzalkowski@gmail.com>
2019-11-17 17:22:51 -0800

    set explicit version requirements for ios and swift

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "Flow",
+    platforms: [
+        // TODO support more platforms
+        .iOS(.v13),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
@@ -24,5 +28,6 @@ let package = Package(
         .testTarget(
             name: "FlowTests",
             dependencies: ["Flow"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
